3db246dd7033c9d25dee8d6f8945d1abdae49dea
Norhan <o-nourhan.said>
2020-10-15 22:14:39 +0200

    - generic functions

--- app/src/main/java/com/example/funwithkotlin/oo/enerics.kt
+++ app/src/main/java/com/example/funwithkotlin/oo/enerics.kt
@@ -2,6 +2,7 @@ package com.example.funwithkotlin.oo
 
 import java.util.*
 
+//generic class
 class TimeLine<E> {
 
     val date2Date: MutableMap<Date, E> = mutableMapOf()
@@ -15,10 +16,21 @@ class TimeLine<E> {
     }
 }
 
+//generic fun
+fun <T> timeLineOf(vararg elements: T): TimeLine<T> {
+    val result = TimeLine<T>()
+    for (element in elements) {
+        result.add(element)
+    }
+    return result
+}
+
 fun main() {
 
     val timeLine: TimeLine<Int> = TimeLine()
 
     timeLine.add(2)
     timeLine.getTheLatest()
+
+    val timeLine2 = timeLineOf("hi", "bye")
 }
